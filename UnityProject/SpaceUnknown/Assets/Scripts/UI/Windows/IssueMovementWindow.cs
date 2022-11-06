865d8f761aea35a628d10e734569efea788fb5e0
Ryan <ryanrothweiler@gmail.com>
2022-11-06 15:20:41 -0800

    asdf

--- UnityProject/SpaceUnknown/Assets/Scripts/UI/Windows/IssueMovementWindow.cs
+++ UnityProject/SpaceUnknown/Assets/Scripts/UI/Windows/IssueMovementWindow.cs
@@ -15,6 +15,9 @@ public class IssueMovementWindow : MonoBehaviour
 	private bool hasDestination;
 	private UniversalPosition destination;
 
+	private float distanceMiles;
+	private float dataTime;
+
 	void Update()
 	{
 		if (RytInput.leftTouch.onUp && !RytInput.leftTouch.moved) {
@@ -23,12 +26,6 @@ public class IssueMovementWindow : MonoBehaviour
 
 		if (hasDestination) {
 			DrawScreenLine.Draw(ship.physics.pos, destination, Color.green);
-
-			float distanceMiles = Vector2.Distance(ship.physics.pos.Get(), destination.Get());
-
-			time.data.text = "";
-			fuel.data.text = "";
-			distance.Set(distanceMiles.ToString("0.0"), "mi");
 		}
 	}
 
@@ -44,7 +41,6 @@ public class IssueMovementWindow : MonoBehaviour
 
 	public void SetDestination(UniversalPosition uniPos)
 	{
-
 		hasDestination = true;
 		destination = uniPos;
 
@@ -53,5 +49,20 @@ public class IssueMovementWindow : MonoBehaviour
 		fuel.gameObject.SetActive(true);
 		time.gameObject.SetActive(true);
 		distance.gameObject.SetActive(true);
+
+		// Calculate datas
+		distanceMiles = Vector2.Distance(ship.physics.pos.Get(), destination.Get());
+		distance.Set(distanceMiles.ToString("0.0"), "mi");
+
+		/*
+		fuel.data.text = "";
+
+		float dataTime = 0;
+		Ship.Physics physics = ship.physics;
+		while (Ship.SimulateMovement(ref physics, ship.def, ship.TotalMass(), destination)) {
+			dataTime++;
+		}
+		time.Set(dataTime.ToString("0.0", "s"));
+		*/
 	}
 }
