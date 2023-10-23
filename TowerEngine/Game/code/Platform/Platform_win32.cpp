c6879f2548e3b508c8683a9e82dd60d7f86c0eff
Ryan <ryanrothweiler@gmail.com>
2023-10-22 17:40:35 -0700

    asdf

--- TowerEngine/Game/code/Platform/Platform_win32.cpp
+++ TowerEngine/Game/code/Platform/Platform_win32.cpp
@@ -1118,7 +1118,7 @@ int32 Run(int32 WindowWidth, int32 WindowHeight, HWND Window, bool32 Headless)
 
 		engine_state *GameStateFromMemory = (engine_state *)GameMemory.PermanentMemory.Memory;
 
-		GameStateFromMemory->SaveDataFolder = "";
+		GameStateFromMemory->SaveDataFolder = "T:/Game/build/Windows";
 
 		// Getting mouse position
 		if (Window != GameNull) {
